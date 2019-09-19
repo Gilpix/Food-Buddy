6649a22c9d85f7a06b5385b1e90d8d2a8a26d8ea
Gilpix <kuldeepsingh5005@gmail.com>
2019-09-19 17:43:03 -0400

    Reset password authentication activity created

---
+++ app/src/main/java/com/kulartist/foodbuddy/ResetPassAuthenticate.java
@@ -0,0 +1,17 @@
+package com.kulartist.foodbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResetPassAuthenticate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pass_authenticate);
+    }
+
+    public void resetPasswordAuthenticate(View view) {
+    }
+}
