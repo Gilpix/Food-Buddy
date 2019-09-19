9eb3e6189139990a4561baadf9151bbbb7480464
Gilpix <kuldeepsingh5005@gmail.com>
2019-09-19 16:48:08 -0400

    First commit

---
+++ app/src/main/java/com/kulartist/foodbuddy/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.kulartist.foodbuddy;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+//    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    String email,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+
+ //       mLoginFormView = findViewById(R.id.login_form);
+
+        // Store values at the time of the login attempt.
+         email = mEmailView.getText().toString();
+         password = mPasswordView.getText().toString();
+        Toast.makeText(this,password+" - Wrong - "+email,Toast.LENGTH_LONG).show();
+
+
+    }
+
+    public void signIn(View view) {
+
+       // if(email.equals("a") && password.equals("a"))
+        {
+            Intent i =new Intent(LoginActivity.this,SplashScreen.class);
+            startActivity(i);
+            finish();
+        }
+      //  else
+           // Toast.makeText(this,password+" Wrong "+email,Toast.LENGTH_LONG).show();
+
+    }
+
+    public void signUp(View view) {
+        Intent i =new Intent(LoginActivity.this,Registration.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void resetPasswordAuth(View view) {
+    }
+}
+
