4305b99c7850bdda9489368483127cec93d20683
Gilpix <kuldeepsingh5005@gmail.com>
2019-10-03 16:58:20 -0400

    DatabaseConnection class created for creating connection between app and mysql database using jdbc driver

---
+++ app/src/main/java/com/kulartist/database_connection/DatabaseConnection.java
@@ -0,0 +1,51 @@
+package com.kulartist.database_connection;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+
+public class DatabaseConnection {
+
+
+    String classs = "com.mysql.jdbc.Driver";
+
+    //a@gmail.com
+
+    String url = "jdbc:mysql://192.168.64.2/FoodBuddy";
+    String un = "fby";
+    String password = "1234";
+
+
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+
+            Class.forName(classs);
+
+            conn = DriverManager.getConnection(url, un, password);
+
+
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+}
