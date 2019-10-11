9dab1ce251e612f56a94445437e8807aecdb3022
Gilpix <kuldeepsingh5005@gmail.com>
2019-10-11 15:08:59 -0400

    settings layout designed, rate us and share our app features added

--- app/src/main/java/com/kulartist/home/Settings.java
+++ app/src/main/java/com/kulartist/home/Settings.java
@@ -1,8 +1,12 @@
 package com.kulartist.home;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 //import android.support.design.widget.FloatingActionButton;
 //import android.support.design.widget.Snackbar;
 
@@ -41,4 +45,48 @@ public class Settings extends Home {
     }
 
 
+    public void rateUs(View view) {
+
+        try
+        {
+            Intent rateIntent = rateIntentForUrl("market://details");
+            startActivity(rateIntent);
+        }
+        catch (ActivityNotFoundException e)
+        {
+            Intent rateIntent = rateIntentForUrl("https://play.google.com/store/apps/details");
+            startActivity(rateIntent);
+        }
+
+    }
+
+
+    private Intent rateIntentForUrl(String url)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("%s?id=%s", url, getPackageName())));
+        int flags = Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK;
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            flags |= Intent.FLAG_ACTIVITY_NEW_DOCUMENT;
+        }
+        else
+        {
+            //noinspection deprecation
+            flags |= Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET;
+        }
+        intent.addFlags(flags);
+        return intent;
+    }
+
+
+    public void shareUs(View view) {
+
+        Intent shareIntent =   new Intent(android.content.Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,"Install This Application");
+        String app_url = "https://play.google.com/store/apps/details?id=com.vid_lancer.trafficroadsignscanada";
+        shareIntent.putExtra(android.content.Intent.EXTRA_TEXT,app_url);
+        startActivity(Intent.createChooser(shareIntent, "Share via"));
+
+    }
 }
